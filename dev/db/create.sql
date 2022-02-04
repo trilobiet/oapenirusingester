daf5d58c323d7af9ed2c9c5b155181cd63648447
acdhirr <acdhirr@trilobiet.nl>
2022-02-04 21:47:10 +0100

    Added DB tables for users and libraries

--- dev/db/create.sql
+++ dev/db/create.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 8.0.27, for Linux (x86_64)
 --
--- Host: 127.0.0.1    Database: irusuk
+-- Host: 104.248.34.253    Database: irusuk
 -- ------------------------------------------------------
--- Server version	8.0.27-0ubuntu0.20.04.1
+-- Server version	8.0.28-0ubuntu0.20.04.3
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -34,8 +34,9 @@ CREATE TABLE `event` (
   `requests` int DEFAULT '0',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `inetAton` varchar(2) DEFAULT NULL,
   PRIMARY KEY (`item_id`,`ip`,`date`),
-  KEY `fk_event_1_idx` (`item_id`),
+  KEY `fk_event_1_idx` (`item_id`,`latitude`,`longitude`,`ip`,`date`),
   CONSTRAINT `fk_event_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -58,10 +59,10 @@ CREATE TABLE `funder` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `ipLocation`
+-- Table structure for table `iplocation`
 --
 
-DROP TABLE IF EXISTS `ipLocation`;
+DROP TABLE IF EXISTS `iplocation`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `iplocation` (
@@ -125,12 +126,57 @@ CREATE TABLE `item_funder` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping events for database 'irusuk'
+-- Table structure for table `library_ips`
 --
 
+DROP TABLE IF EXISTS `library_ips`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `library_ips` (
+  `library_id` varchar(36) NOT NULL,
+  `ip1` varchar(40) NOT NULL,
+  `ip2` varchar(40) NOT NULL,
+  `ip1a` bigint DEFAULT NULL,
+  `ip2a` bigint DEFAULT NULL,
+  PRIMARY KEY (`library_id`,`ip1`,`ip2`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`trilobiet`@`83.163.15.48`*/ /*!50003 TRIGGER `before_insert_library_ips` BEFORE INSERT ON `library_ips` FOR EACH ROW SET 
+  new.ip1a = inet_aton(new.ip1),
+  new.ip2a = inet_aton(new.ip2) */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
 --
--- Dumping routines for database 'irusuk'
+-- Table structure for table `user`
 --
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` varchar(36) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `role` enum('funder','publisher','library') NOT NULL,
+  `irusId` varchar(36) DEFAULT NULL,
+  `lat` double DEFAULT '0',
+  `lon` double DEFAULT '0',
+  `country_code` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -141,4 +187,4 @@ CREATE TABLE `item_funder` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-25 11:17:09
+-- Dump completed on 2022-02-04 21:45:08
